582876b8f1afae92571c0b5b02ccaafc7d257542
RajeshwariSelvaraj <rajeshwariselvaraj929@gmail.com>
2026-08-09 11:27:30 +0530

    Add an element to the array in traversal.java

--- traversal.java
+++ traversal.java
@@ -2,7 +2,7 @@ package Day06;
 public class traversal {
 
 	public static void main(String[] args) {
-		int[] arr = {10, 20, 30, 40};
+		int[] arr = {10, 20, 30, 40,50};
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
