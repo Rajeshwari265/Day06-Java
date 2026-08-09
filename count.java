4d8a8f4fad6208c09ca567d73c304e6a9d4bae17
RajeshwariSelvaraj <rajeshwariselvaraj929@gmail.com>
2026-08-09 11:22:30 +0530

    Change array elements in count.java

--- count.java
+++ count.java
@@ -3,7 +3,7 @@ package Day06;
 public class count {
 
 	public static void main(String[] args) {
-		int[] arr = {2, 3, 2, 4, 2, 5};
+		int[] arr = {2, 3, 2, 3, 2, 5};
 
         int search = 2;
         int count = 0;
