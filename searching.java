f5896d54f1220e37b1e0b5e908da79cf9762c50d
RajeshwariSelvaraj <rajeshwariselvaraj929@gmail.com>
2026-08-09 11:25:35 +0530

    Change search element from 30 to 40

--- searching.java
+++ searching.java
@@ -5,7 +5,7 @@ public class searching {
 	public static void main(String[] args) {
 		int[] arr = {10, 20, 30, 40, 50};
 
-        int search = 30;
+        int search = 40;
         boolean found = false;
 
         for (int i = 0; i < arr.length; i++) {
